f15f5ee32118a6c7223684acef76aedab71df25e
Salman Ahmad <salman@salmanahmad.com>
2014-02-25 11:44:15 -0500

    Adding HTTP benchmarks.

---
+++ http/hello-world/ruby/server.rb
@@ -0,0 +1,12 @@
+
+require 'rack'
+require 'thin'
+
+class HelloWorld
+  def call(env)
+    [200, {"Content-Type" => "text/plain"}, "Hello, World!"]
+  end
+end
+
+Rack::Handler::Thin.run HelloWorld.new, :Port => 8000
+
