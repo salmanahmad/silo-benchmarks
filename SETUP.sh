5c6b676e8d0b26629c379b46c5baa837274524ac
Salman Ahmad <salman@salmanahmad.com>
2014-03-04 00:44:54 -0500

    Updating the SETUP file to fix port ranges.

--- SETUP.sh
+++ SETUP.sh
@@ -1,4 +1,14 @@
 
+# Increase Port Range (Default: 49152)
+sudo sysctl -w net.inet.ip.portrange.first=32768
+
+# Increase MSL and TIME_WAIT (Default: 15000)
+sudo sysctl -w net.inet.tcp.msl=1000
+
+# Increase ulimit
+sudo ulimit -n 10240
+
+# Taken from Benchmarking Python Servers Link
 #echo "10152 65535" > /proc/sys/net/ipv4/ip_local_port_range
 #sysctl -w fs.file-max=128000
 #sysctl -w net.ipv4.tcp_keepalive_time=300
