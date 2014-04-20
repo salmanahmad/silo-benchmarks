11520dce9a3cf369a58e13910735eb9f2f2c238d
Salman Ahmad <salman@salmanahmad.com>
2014-04-20 10:11:25 -0400

    Updating generator.

--- actor/fibonacci-generator/akka/src/main/java/Main.java
+++ actor/fibonacci-generator/akka/src/main/java/Main.java
@@ -2,6 +2,7 @@ import akka.actor.ActorRef;
 import akka.actor.UntypedActor;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.japi.Creator;
 import akka.actor.Inbox;
 import scala.concurrent.duration.Duration;
 import scala.concurrent.duration.FiniteDuration;
@@ -10,6 +11,10 @@ import java.io.Serializable;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
+    public static class Start implements Serializable {}
+
+    public static class Stop implements Serializable {}
+
     public static class Message implements Serializable {
         public final int value;
         public Message(int value) {
@@ -30,6 +35,43 @@ public class Main {
         }
     }
 
+    public static class Tester extends UntypedActor {
+        public static Props props(final ActorRef generator) {
+            return Props.create(new Creator<Tester>() {
+                private static final long serialVersionUID = 1L;
+ 
+                @Override
+                public Tester create() throws Exception {
+                  return new Tester(generator);
+                }
+            });
+        }
+
+        final ActorRef generator;
+
+        ActorRef starter;
+        int count = 1000000;
+
+        public Tester(ActorRef generator) {
+            this.generator = generator;
+        }
+
+        public void onReceive(Object message) {
+            if(message instanceof Start) {
+                starter = getSender();
+                generator.tell(new Message(0), getSelf());
+            } else if(message instanceof Message) {
+                if(count == 0) {
+                    System.out.println("Stopping!");
+                    starter.tell(new Stop(), getSelf());
+                } else {
+                    this.count = this.count - 1;
+                    generator.tell(new Message(0), getSelf());
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         long startTime = System.nanoTime();
 
@@ -37,11 +79,14 @@ public class Main {
         final Inbox inbox = Inbox.create(system);
 
         ActorRef generator = system.actorOf(Props.create(Fib.class), "generator");
+        ActorRef tester = system.actorOf(Tester.props(generator), "tester");
 
-        for(int i = 0; i < 1000000; i++) {
-            inbox.send(generator, new Message(0));
-            Message greeting1 = (Message)inbox.receive(Duration.create(5, TimeUnit.SECONDS));
-        }
+        System.out.println("Starting!");
+
+        inbox.send(tester, new Start());
+        inbox.receive(Duration.create(5, TimeUnit.MINUTES));
+
+        System.out.println("Done!");
 
         long estimatedTime = System.nanoTime() - startTime;
         System.out.println("Total Duration: " + (estimatedTime / 1000000));
