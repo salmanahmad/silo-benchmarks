a1ec6ec384768defe1189b280a14c5b677ad060c
Salman Ahmad <salman@salmanahmad.com>
2014-04-20 09:56:56 -0400

    Fixing the akka sample.

--- actor/ping-pong/akka/src/main/java/Main.java
+++ actor/ping-pong/akka/src/main/java/Main.java
@@ -2,6 +2,7 @@ import akka.actor.ActorRef;
 import akka.actor.UntypedActor;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.japi.Creator;
 import akka.actor.Inbox;
 import scala.concurrent.duration.Duration;
 import scala.concurrent.duration.FiniteDuration;
@@ -12,16 +13,64 @@ import java.io.Serializable;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static class Message implements Serializable {}
+    public static class Start implements Serializable {}
 
-    public static class Ponger extends UntypedActor {
+    public static class Stop implements Serializable {}
+
+    public static class Ping implements Serializable {}
+
+    public static class Pong implements Serializable {}
+
+    public static class Pinger extends UntypedActor {
+        public static Props props(final ArrayList actors) {
+            return Props.create(new Creator<Pinger>() {
+                private static final long serialVersionUID = 1L;
+ 
+                @Override
+                public Pinger create() throws Exception {
+                  return new Pinger(actors);
+                }
+            });
+        }
+
+        public static int random(int min, int max) {
+            return min + (int)(Math.random() * ((max - min) + 1));
+        }
+
+        final ArrayList actors;
+        int count = 1000000;
+        ActorRef starter = null;
+
+        public Pinger(ArrayList actors) {
+            this.actors = actors;
+        }
+
         public void onReceive(Object message) {
-            getSender().tell(new Message(), getSelf());
+            if(message instanceof Pong) {
+                if(count == 0) {
+                    System.out.println("Stopping!");
+                    starter.tell(new Stop(), getSelf());
+                } else {
+                    this.count = this.count - 1;
+
+                    ActorRef actor = (ActorRef)actors.get(random(0, actors.size() - 1));
+                    actor.tell(new Ping(), getSelf());
+                }
+            } else if(message instanceof Start) {
+                starter = getSender();
+
+                ActorRef actor = (ActorRef)actors.get(random(0, actors.size() - 1));
+                actor.tell(new Ping(), getSelf());
+            }
         }
     }
 
-    public static int random(int min, int max) {
-        return min + (int)(Math.random() * ((max - min) + 1));
+    public static class Ponger extends UntypedActor {
+        public void onReceive(Object message) {
+            if(message instanceof Ping) {
+                getSender().tell(new Pong(), getSelf());
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -33,15 +82,17 @@ public class Main {
         ArrayList actors = new ArrayList();
 
         for(int i = 0; i < 100; i++) {
-            actors.add(system.actorOf(Props.create(Fib.class), "generator" + i));
+            actors.add(system.actorOf(Props.create(Ponger.class), "ponger" + i));
         }
 
-        for(int i = 0; i < 1000000; i++) {
-            ActorRef actor = actors.get(random(0, 100 - 1));
+        ActorRef actor = system.actorOf(Pinger.props(actors), "pinger");
 
-            inbox.send(actor, new Message());
-            inbox.receive(Duration.create(5, TimeUnit.SECONDS));
-        }
+        System.out.println("Starting!");
+
+        inbox.send(actor, new Start());
+        inbox.receive(Duration.create(5, TimeUnit.MINUTES));
+
+        System.out.println("Done!");
 
         long estimatedTime = System.nanoTime() - startTime;
         System.out.println("Total Duration: " + (estimatedTime / 1000000));
